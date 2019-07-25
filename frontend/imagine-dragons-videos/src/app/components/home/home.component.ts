6e3c83eb466cdaab2e3c9c408a07188b9ba1527c
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-07-25 23:51:07 +0200

    CSS of Home Component started.

--- frontend/imagine-dragons-videos/src/app/components/home/home.component.ts
+++ frontend/imagine-dragons-videos/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {VideoService} from '../../services/video.service';
 import {Video} from '../../models/Video';
 
@@ -10,10 +10,26 @@ import {Video} from '../../models/Video';
 export class HomeComponent implements OnInit {
   videos: Video[];
 
-  constructor(private videoService: VideoService) { }
+  constructor(private videoService: VideoService) {
+  }
 
   ngOnInit() {
     this.videoService.getVideos().subscribe(response => this.videos = response);
+    setInterval(() => {this.movePictures(); }, 15000);
+  }
+
+  movePictures() {
+    const pic1 = document.getElementById('carousel-img1');
+    const pic2 = document.getElementById('carousel-img2');
+    const pic3 = document.getElementById('carousel-img3');
+    const pics = [pic3, pic2, pic1];
+    console.log('called');
+
+    setTimeout(() => pic1.style.display = 'none', 5000);
+    setTimeout(() => pic2.style.display = 'none', 10000);
+    setTimeout(() => {
+      pics.forEach(pic => pic.style.display = 'block');
+    }, 15000);
   }
 
 }
